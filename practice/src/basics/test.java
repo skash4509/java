6cd4291ea28c3d1a12c19e8eecfc60c95e721b1e
Shubham <Shubham@192.168.0.247>
2022-10-18 21:32:54 +0530

    java

---
+++ practice/src/basics/test.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class test 
+{
+	public static void main(String[] args) 
+	{
+		int a=5;
+		int b=5;
+		int c=a+b;
+		System.out.println(c);
+	}
+}
--- practice/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author Shubham
- *
- */
-module practice {
-}
